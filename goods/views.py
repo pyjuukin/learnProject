6311627821e5ff25e8298867c1e7d900c31d7083
pyjuukin <pyjuukin@gmail.com>
2024-03-16 19:21:19 +0300

    add slug

--- goods/views.py
+++ goods/views.py
@@ -15,5 +15,16 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug=False, product_id=False):
+
+
+    if product_id:
+        product = Products.objects.get(id=product_id)
+    else:
+        product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, "goods/product.html", context=context)
